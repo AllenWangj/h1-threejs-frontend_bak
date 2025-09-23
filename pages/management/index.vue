afe2d4d8e0de4daa8d90f7dc27cd795b09498323
WIN-1ETVCJSERNV\Administrator <805822426@qq.com>
2025-09-23 22:27:13 +0800

    feat:模型库开发完成

--- pages/management/index.vue
+++ pages/management/index.vue
@@ -1,71 +1,61 @@
 <template>
   <div>
-    <ez-fast-table
-      ref="tableRef"
-      v-model:params="searchParams"
-      v-model:loading="loading"
-      :columns="columns"
-      :api="getStudentSettledPage"
-      :max-height="tableMaxHeight"
-    >
+    <ez-fast-table ref="tableRef" v-model:params="searchParams" v-model:loading="loading" :columns="columns"
+      :api="getStudentSettledPage" :max-height="tableMaxHeight">
       <template #form>
         <el-form :model="searchParams" inline label-width="auto" @submit.prevent @keyup.enter="search">
-          <el-form-item label="模型名称">
-            <el-input v-model="searchParams.name" clearable />
-          </el-form-item>
+          <el-col :span="8">
+            <el-form-item label="模型名称">
+              <el-input v-model="searchParams.name" clearable />
+            </el-form-item>
+          </el-col>
         </el-form>
       </template>
       <template #header>
         <el-button type="primary" :icon="Plus" @click="addItem">新增</el-button>
       </template>
       <template #operation="{ row }">
-        <el-button link type="primary" @click="viewItem(row)">查看</el-button>
+        <el-button link type="primary" @click="viewItem(row)">查看模型</el-button>
         <el-button link type="primary" @click="editItem(row)">编辑</el-button>
         <el-button link type="primary" @click="deleteItem(row)">删除</el-button>
       </template>
-    </ez-fast-table>
+        <template #createTime="{ row }">
+          {{ dayjs(row.createTime).format('YYYY-MM-DD HH:mm:ss') }}
+        </template>
+        <template #length="{ row }">
+          {{ row.length }}mm
+        </template>
+        <template #width="{ row }">
+                   {{ row.width }}mm
 
-    <ez-dialog v-model="dialogVisible" title="新增模型">
-      <el-form ref="formRef" :model="formData" label-width="100px" :rules="rules">
-        <el-form-item label="模型名称" prop="name">
-          <el-input v-model="formData.name" clearable />
-        </el-form-item>
-        <el-form-item label="模型地址" prop="url">
-          <!-- <el-input v-model="formData.url" clearable /> -->
-          <p v-if="formData.url" class="mr-[20px]">{{ formData.url }}</p>
-          <el-upload
-            ref="uploadRef"
-            accept=".zip"
-            action=""
-            :auto-upload="false"
-            :on-change="uploadFile"
-            :before-remove="removeFile"
-            :show-file-list="false"
-          >
-            <el-button type="primary" :disabled="uploadLoading" :loading="uploadLoading">
-              {{ uploadLoading ? '上传中...' : '点击上传' }}
-            </el-button>
-          </el-upload>
-        </el-form-item>
-        <el-form-item label="备注" prop="remark">
-          <el-input v-model="formData.remark" type="textarea" clearable />
-        </el-form-item>
-      </el-form>
-    </ez-dialog>
+        </template>
+        <template #height="{ row }">
+             {{ row.height }}mm
+        </template>
+    </ez-fast-table>
+    <UpdateModal @register="register" @success="handleSuccessEvt"/>
+    <ShowModeModal @register="registerOfShow"/>
+
   </div>
 </template>
-
 <script setup lang="ts">
 import { Plus } from '@maxtan/ez-ui-icons'
 import type { SelectOption, TableColumn } from '@maxtan/ez-ui'
-import { useUpload } from '~/composables/use-upload'
+import UpdateModal from "./update-modal.vue"
+import {useModal} from "~~/component/BaseModel/index"
+import { getModelList } from '~/apis/model'
+import dayjs from 'dayjs';
+import ShowModeModal from "./show-modal.vue"
+import { removeModel} from '~/apis/model'
+
+// getModelList
 definePageMeta({
   layout: 'management',
   permissions: 'management'
 })
-
 const router = useRouter()
-const { fetchOssAuth, loadFile } = useUpload()
+const [register,{openModal}] = useModal()
+const [registerOfShow,{openModal:opennShowModal}] = useModal()
 
 const loading = ref(false)
 const { formatTime } = useUtils()
@@ -75,19 +65,33 @@ const searchParams = reactive({
   current: 1,
   size: 10
 })
-
 const columns = reactive<TableColumn<any>[]>([
   { prop: 'name', label: '模型名称', width: 200, fixed: 'left', sortable: true },
+  // {
+  //   prop: 'url',
+  //   label: '模型地址'
+  // },
   {
-    prop: 'previewUrl',
-    label: '预览地址'
+    prop: 'length',
+    label: '长度'
   },
   {
+    prop: 'width',
+    label: '宽度'
+  },
+  {
+    prop: 'height',
+    label: '高度'
+  },
+   {
     prop: 'createTime',
     label: '创建时间'
   },
   { prop: 'operation', label: '操作', width: 200, fixed: 'right' }
 ])
+function handleSuccessEvt(){
+  search()
+}
 const tableRef = ref()
 /**
  * 搜索
@@ -101,32 +105,50 @@ const search = () => {
  * 新增
  */
 const addItem = () => {
-  dialogVisible.value = true
+  // dialogVisible.value = true
+  openModal(true,{
+     update:false,
+  })
 }
 
 /**
  * 查看
  */
 const viewItem = (_item) => {
-  router.push(`/student/bank/detail?id=${_item.id}`)
+  //查看模型
+  opennShowModal(true,{
+    record:_item
+  })
+  // router.push(`/student/bank/detail?id=${_item.id}`)
 }
 
 /**
  * 编辑
  */
 const editItem = (_item) => {
-  router.push(`/student/bank/edit?id=${_item.id}`)
+  // router.push(`/student/bank/edit?id=${_item.id}`)
+    openModal(true,{
+      update:true,
+      record:_item
+    })
 }
 
 /**
  * 删除
  */
-const deleteItem = (_item) => {
+const deleteItem = async (_item) => {
   // 删除逻辑
+    try {
+    const confirm = await ElMessageBox.confirm('是否删除?', '提示')
+    if (confirm === 'confirm') {
+      await removeModel({ id: _item.id })
+      ElMessage.success('操作成功！')
+      search()
+    }
+  } catch (error) {}
 }
 
 onMounted(() => {
-  fetchOssAuth()
   search()
 })
 
@@ -135,109 +157,10 @@ onMounted(() => {
  */
 const getStudentSettledPage = async (params) => {
   return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        code: 200,
-        data: {
-          size: 10,
-          current: 1,
-          records: [
-            {
-              createTime: 1730988193411,
-              createBy: '2d236942518d35cd',
-              updateTime: 1754875131321,
-              updateBy: '2d236942518d35cd',
-              id: '2d236942518d35cd',
-              name: null,
-              password: null,
-              email: null,
-              realName: '谭鹏',
-              avatar: null,
-              status: 0,
-              mobile: 'xxxx',
-              source: 1,
-              birth: null,
-              gender: null,
-              province: null,
-              city: null,
-              country: null
-            }
-          ],
-          total: 1,
-          pages: 1
-        },
-        message: '成功',
-        success: true,
-        timestamp: 1754879682677
-      })
-    }, 1000)
+    getModelList(params).then(res=>{
+      resolve(res)
+    })
   })
 }
 
-// 新增对话框显示控制
-const dialogVisible = ref(false)
-
-const formData = reactive({
-  name: '',
-  url: 'wwww.baidu.com',
-  remark: ''
-})
-const rules = reactive({
-  name: [{ required: true, message: '请输入模型名称', trigger: 'blur' }],
-  url: [{ required: true, message: '请输入模型地址', trigger: 'blur' }]
-})
-const { uploadRef, uploadLoading, uploadFile, removeFile } = createdUploadFile()
-
-/**
- * 创建文件上传相关方法和状态
- * @function createdUploadFile
- * @returns {Object} 上传文件相关的方法和状态
- */
-function createdUploadFile() {
-  const uploadRef = ref(null)
-  const uploadLoading = ref(false)
-
-  /**
-   * 处理文件上传
-   * @param {File} file - 上传的文件对象
-   */
-  const uploadFile = (file: any) => {
-    formData.name = file.name
-    submitFile(file)
-  }
-
-  /**
-   * 移除已上传的文件
-   */
-  const removeFile = () => {
-    uploadRef.value?.clearFiles()
-    return true
-  }
-
-  /**
-   * 提交文件到服务器
-   * @param {File} file - 要上传的文件
-   */
-  const submitFile = async (file: any) => {
-    try {
-      uploadLoading.value = true
-      const { url } = await loadFile(file.raw)
-      ElMessage.success('上传成功')
-      formData.url = url
-    } catch (error) {
-      formData.name = ''
-      formData.url = ''
-    } finally {
-      uploadLoading.value = false
-      removeFile()
-    }
-  }
-
-  return {
-    uploadRef,
-    uploadLoading,
-    uploadFile,
-    removeFile
-  }
-}
 </script>
---
+++ pages/management/show-modal.vue
@@ -0,0 +1,78 @@
+<template>
+  <BasicModal @register="register" width="1200px" title="上传模型" :before-close="beforeCloseEvt"
+    :close-on-click-modal="false">
+    <div class="show-modal-wrapper" v-loading="loading" :element-loading-text="message">
+      <div class="show-modal-three" ref="three"></div>
+      <div class="model-info">
+         <el-descriptions title="模型信息" :column="1">
+           <el-descriptions-item label="长度" :span="1" width="140px">{{  boxReactive.width.toFixed(2) }}mm</el-descriptions-item>
+           <el-descriptions-item label="宽度" :span="1" width="140px">{{  boxReactive.height .toFixed(2)}}mm</el-descriptions-item>
+           <el-descriptions-item label="高度" :span="1" width="140px">{{  boxReactive.deepth.toFixed(2) }}mm</el-descriptions-item>
+
+          </el-descriptions>
+      </div>
+    </div>
+    <template #footer>
+    </template>
+  </BasicModal>
+</template>
+<script setup lang="ts">
+import { BasicModal, useInnnerModal } from "~~/component/BaseModel/index"
+import Libary from "~~/threejs/libary/index"
+import { ref ,reactive} from "vue"
+const three = ref()
+const loading = ref(false)
+const message = ref("正在加载")
+let optObj :Libary | null = null
+const boxReactive = reactive({
+  width:0,
+  height:0,
+  deepth:0,
+})
+const [register, { closeModal }] = useInnnerModal((data) => {
+  loading.value = true
+  setTimeout(() => {
+    const obj = new Libary(three.value, {
+      progress: (progress) => {
+        message.value = `正在加载${progress}%`
+      }
+    })
+    obj.handleLoadModelByUrl(data.record.url,(box) =>{
+      boxReactive.width= box.width
+      boxReactive.height= box.height
+      boxReactive.deepth= box.len
+    }).then(() => {
+      loading.value = false
+    }, () => {
+      loading.value = false
+
+    })
+    optObj = obj
+  }, 100)
+})
+function beforeCloseEvt() {
+  closeModal()
+  optObj!.handleDestoryResource()
+}
+
+</script>
+<style lang="less" scoped>
+.show-modal-wrapper {
+  width: 100%;
+  height: 50vh;
+  position: relative;
+
+  .show-modal-three {
+    width: 100%;
+    height: 100%;
+  }
+  .model-info {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background: #ffff;
+    padding:20px
+  }
+
+}
+</style>
